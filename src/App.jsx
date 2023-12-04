ff0bc1f6bb1c94ac9e36bbb609a1c8b4900cb5c0
Folka2134 <folka2134@gmail.com>
2023-12-04 10:47:35 +0000

    added navigation to game pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { GiCardBurn } from "react-icons/gi";
 import { CiMedicalCross } from "react-icons/ci";
 import { VscWholeWord } from "react-icons/vsc";
 import { FaQuestion } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -11,18 +12,30 @@ function App() {
       <div className="flex min-h-screen flex-col items-center justify-center p-12 md:p-24">
         <h1 className="mb-5 text-3xl text-pink-500">Mini-Gamies</h1>
         <div className="grid h-52 w-64 grid-cols-2 justify-items-center gap-5">
-          <span className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80">
+          <Link
+            to="/card-match"
+            className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80"
+          >
             <GiCardBurn size={70} />
-          </span>
-          <span className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80">
+          </Link>
+          <Link
+            to="/tic-tac-toe"
+            className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80"
+          >
             <CiMedicalCross size={70} />
-          </span>
-          <span className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80">
+          </Link>
+          <Link
+            to="/word-search"
+            className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80"
+          >
             <VscWholeWord size={70} />
-          </span>
-          <span className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80">
+          </Link>
+          <Link
+            to="/trivia"
+            className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80"
+          >
             <FaQuestion size={70} />
-          </span>
+          </Link>
         </div>
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Trivia from "./components/Trivia.jsx";
+import WordSearch from "./components/WordSearch.jsx";
+import Tictactoe from "./components/Tictactoe.jsx";
+import CardMatch from "./components/CardMatch.jsx";
 
 import App from "./App.jsx";
 import "./index.css";
@@ -9,6 +13,22 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/card-match",
+    element: <CardMatch />,
+  },
+  {
+    path: "/tic-tac-toe",
+    element: <Tictactoe />,
+  },
+  {
+    path: "/word-search",
+    element: <WordSearch />,
+  },
+  {
+    path: "/trivia",
+    element: <Trivia />,
   },
 ]);
 
