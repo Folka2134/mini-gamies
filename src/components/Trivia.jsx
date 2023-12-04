0cbf43ad44c37637b905a66ab87e942e0cdfe631
Folka2134 <folka2134@gmail.com>
2023-12-04 10:51:17 +0000

    added links back to root

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Trivia = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center p-12 md:p-24">
+      <h1 className="absolute top-12 mb-5 text-3xl text-pink-500 md:top-24">
+        <Link to="/">Mini-Gamies</Link>
+      </h1>
       <span>Trivia</span>
     </div>
   );
