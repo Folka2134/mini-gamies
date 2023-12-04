9459d9c6cc3b2e4abd1b0697d8745e0bd28a080e
Folka2134 <folka2134@gmail.com>
2023-12-04 10:38:32 +0000

    updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center p-12 md:p-24">
-        <h1 className="mb-16 text-3xl text-pink-500">Mini-Gamies</h1>
+        <h1 className="mb-5 text-3xl text-pink-500">Mini-Gamies</h1>
         <div className="grid h-52 w-64 grid-cols-2 justify-items-center gap-5">
           <span className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl transition-all duration-300 hover:animate-pulse hover:bg-white hover:bg-opacity-80">
             <GiCardBurn size={70} />
